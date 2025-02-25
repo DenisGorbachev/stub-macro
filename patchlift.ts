cb76957b4e32bc3a5ca6d220073af8063105e8ba
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-25 16:43:07 +0700

    conf: setup patchlift & update configs

--- patchlift.ts
+++ patchlift.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run --allow-write --allow-read --allow-run=bash --allow-net --allow-env --allow-sys --no-lock
 
-import { applyToPublicRustCrateDir } from "jsr:@dengorbachev/patchlift@0.1.6"
+import { applyToPublicRustCrateDir } from "jsr:@dengorbachev/patchlift@0.1.9"
 
 await applyToPublicRustCrateDir(import.meta.dirname)
